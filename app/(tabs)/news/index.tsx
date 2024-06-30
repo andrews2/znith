d609e98af47084c566ae7fcda5e70ee3aa9c4d8e
andrews2 <andrewjshipman1@gmail.com>
2024-06-30 08:02:22 -0400

    basic news view

--- app/(tabs)/news/index.tsx
+++ app/(tabs)/news/index.tsx
@@ -1,18 +1,39 @@
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, FlatList, Image } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { ReactElement, useEffect, useMemo, useState } from "react";
+
+export type ArticleType = {
+  title: string;
+  author: string;
+  url: string;
+  image: string;
+};
 
 export default function TabTwoScreen() {
+  const [articles, setArticles] = useState<ArticleType[]>([]);
+
+  useEffect(() => {
+    if (articles.length === 0) {
+      fetch("https://znith-server.onrender.com/api/news")
+        .then((response) => response.json())
+        .then((data) => setArticles(data.articles));
+    }
+  }, [articles.length]);
+
+  const Item = (props: ArticleType) => {
+    return (
+      <View style={styles.item}>
+        <Image source={{ uri: props.image }} style={styles.image} />
+        <Text>{props.title}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <FlatList data={articles} renderItem={({ item }) => <Item {...item} />} />
     </View>
   );
 }
@@ -22,6 +43,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  item: {
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 5,
   },
   title: {
     fontSize: 20,
